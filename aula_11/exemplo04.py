4e237a3a1ca48750124301a8c5c1029e2e18e845
GharduimT <gabriel.harduim.ti@gmail.com>
2025-05-15 20:00:12 -0300

    banco

---
+++ aula_11/exemplo04.py
@@ -0,0 +1,15 @@
+print('=== AGIOT BANK ===')
+
+try:
+    saldo = 1000
+    saque = float(input('Infomre o Valor para saque: '))
+
+except ValueError as e:
+    print(f'Digite apenas números: {e}')
+
+
+else:
+    if
+
+
+finally:
