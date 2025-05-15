7b40026cec9be677ec399d27d0022a5eae105246
GharduimT <gabriel.harduim.ti@gmail.com>
2025-05-15 19:03:24 -0300

    contagem

--- aula_11/exemplo01.py
+++ aula_11/exemplo01.py
@@ -14,3 +14,6 @@
 
 # div = n1 / n2 #ZeroDivisionError
 # print(div)
+
+lst_num = ['Maria', 'joao', 'Pedro'] #'''  a contagem começa com 0 logo quando é pedido para printar o terceiro item...'''
+print(lst_num[3]) #  IndexError
