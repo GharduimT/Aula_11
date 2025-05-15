6d55749372859e9b5d5a0007514a680104ab3a13
GharduimT <gabriel.harduim.ti@gmail.com>
2025-05-15 18:58:10 -0300

    divisão por zero

---
+++ aula_11/exemplo01.py
@@ -0,0 +1,16 @@
+# print (nome) #name error
+
+# idade = int(input('Digite a idade: '))
+# print(idade)
+
+# n1 = int(input('Informe o número: '))
+# n2 = input('Informe o número: ')
+
+# resultado = n1 + n2  #  TypeError
+# print (resultado)
+
+# n1 = int(input('Informe o número: '))
+# n2 = int(input('Informe o número: '))
+
+# div = n1 / n2 #ZeroDivisionError
+# print(div)
