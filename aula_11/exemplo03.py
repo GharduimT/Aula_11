58ec3a459fcc61d8e001368168a3fd67b66cff99
GharduimT <gabriel.harduim.ti@gmail.com>
2025-05-15 19:41:45 -0300

    Exception

--- aula_11/exemplo03.py
+++ aula_11/exemplo03.py
@@ -1,11 +1,28 @@
+# try: 
+#     n1 = float(input('Numero: '))
+#     n2 = float(input('Numero: '))
+#     resultado = n1 / n2
+
+    
+# except ZeroDivisionError as e:
+#     print(f'Não há divisão por 0 - {e}')
+
+# else:
+#     print(f'O resultado é: {resultado}')
+
+            
+# finally:
+#     print('Fim da Operação!')
+
 try: 
+    
     n1 = float(input('Numero: '))
     n2 = float(input('Numero: '))
     resultado = n1 / n2
 
     
-except ZeroDivisionError as e:
-    print(f'Não há divisão por 0 - {e}')
+except Exception as e:
+    print(f'Erro - {e}')
 
 else:
     print(f'O resultado é: {resultado}')
