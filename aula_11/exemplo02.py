d126091aab7417347afc01840ca1957874ce7cce
GharduimT <gabriel.harduim.ti@gmail.com>
2025-05-15 19:28:30 -0300

    ValueError,TypeError

--- aula_11/exemplo02.py
+++ aula_11/exemplo02.py
@@ -1,9 +1,44 @@
-try: #  TENTE EXECUTAR
+# try: #  TENTE EXECUTAR
 
+#     n1 = float(input('Numero: '))
+#     n2 = float(input('Numero: '))
+#     div = n1 / n2
+#     print(div)
+    
+# except ZeroDivisionError:  #  escreva a mensagem ERROR se tentar dividir por zero
+#     print('Error')
+
+# #2
+# try: 
+#     n1 = float(input('Numero: '))
+#     n2 = float(input('Numero: '))
+#     div = n1 / n2
+#     print(div)
+    
+# except ZeroDivisionError as e: 
+#     print(f'Não pode dividir por 0 - {e}')
+
+#3
+# try: 
+#     n1 = float(input('Numero: '))
+#     n2 = float(input('Numero: '))
+#     div = n1 / n2
+#     print(div)
+    
+# except ZeroDivisionError as e: 
+#     print(f'Não pode dividir por 0 - {e}')
+
+# except ValueError as e:
+#     print(f'Não digite por extenso - {e}')
+
+try: 
     n1 = float(input('Numero: '))
     n2 = float(input('Numero: '))
     div = n1 / n2
     print(div)
     
-except ZeroDivisionError:  #  escreva ERROR se tentar dividir por zero
-    print('Error')
+except (ValueError, TypeError) as e: 
+    print(f'Informe apenas números - {e}')
+
+except ZeroDivisionError as e:
+    print(f'Não há divisão por 0 - {e}')
--- aula_11/tempCodeRunnerFile.py
+++ aula_11/tempCodeRunnerFile.py
@@ -1,1 +1,9 @@
-idade
+try: #  TENTE EXECUTAR
+
+    n1 = float(input('Numero: '))
+    n2 = float(input('Numero: '))
+    div = n1 / n2
+    print(div)
+    
+except ZeroDivisionError
+    print('Error')
