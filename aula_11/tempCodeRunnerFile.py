35877f7b78f6b18a729549e0ae8cc96c06388995
GharduimT <gabriel.harduim.ti@gmail.com>
2025-05-15 20:00:26 -0300

    banco

--- aula_11/tempCodeRunnerFile.py
+++ aula_11/tempCodeRunnerFile.py
@@ -1,9 +1,8 @@
-try: #  TENTE EXECUTAR
+print('=== AGIOT BANK ===')
 
-    n1 = float(input('Numero: '))
-    n2 = float(input('Numero: '))
-    div = n1 / n2
-    print(div)
-    
-except ZeroDivisionError
-    print('Error')
+try:
+    saldo = 1000
+    saque = float(input('Infomre o Valor para saque: '))
+
+except ValueError as e:
+    print(f'Digite apenas números: {e}')
